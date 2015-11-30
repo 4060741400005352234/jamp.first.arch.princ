58d6115ae3ad5b08c3bde40bf62c3490fa53abcb
foxygun <foxygun@bk.ru>
2015-11-30 10:03:36 +0600

    4-5st day.

--- src/com/epam/jamp/main/Main.java
+++ src/com/epam/jamp/main/Main.java
@@ -6,7 +6,6 @@ import org.apache.log4j.Logger;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Main {
 
@@ -19,8 +18,6 @@ public class Main {
 
         DuckController duckController = new DuckController(new BufferedReader(new InputStreamReader(System.in)), new MovableDuckFactory());
         duckController.performControl();
-
-        log.info("All work done.");
 
     }
 }
