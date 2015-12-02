553bf724f5ecc4afad58bd26b5d84d3affaf1176
4060741400005352234 <epam.yevgeniy.popov@gmail.com>
2015-12-02 09:30:34 +0600

    6st day. Maze solving

--- src/com/epam/jamp/main/DuckController.java
+++ src/com/epam/jamp/main/DuckController.java
@@ -7,7 +7,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-// TODO devide this class and rename
 public class DuckController {
 
     private static Logger log = Logger.getLogger(DuckController.class);
